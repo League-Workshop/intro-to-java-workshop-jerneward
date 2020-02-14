0a8e85f1bf94762832e7412b953abbc7f6fd0738
League Student <league@iMac-109.attlocal.net>
2020-02-13 18:23:38 -0800

    Finished Candy Crush & Mad Libs

---
+++ src/section3/Greater.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greater {
+public static void main(String[] args) {
+	System.out.println("Jerne");
+	
+	String age = JOptionPane.showInputDialog("How old are you?");
+JOptionPane.showMessageDialog(null, "I am" + age + "years old");
+
+}
+}
